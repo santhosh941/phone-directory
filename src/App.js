1548dd772d4edafcc3e12e7c15e092b8d62df3a9
santhosh941 <sheshusantu301@gmail.com>
2024-03-26 15:19:55 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@
 function App() {
   return (
     <div>
-    Phone Directory
-    </div>
+        Phone Directory
+        <div className="header">
+          Phone Directory
+        </div>
+        <button>Add</button>
+        <div>
+          <span>Name</span><br />
+          <span>Phone</span>
+        </div>
+      </div>
   );
 }
 
